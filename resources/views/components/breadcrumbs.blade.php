abc66eaa9412126b974295f80ea60121cab99674
Michaelninder <fabianeternis@gmail.com>
2025-07-27 23:15:53 +0200

    updated breadcrumbs

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -5,6 +5,7 @@
         <li class="inline-flex items-center">
             @php
                 $isAdminRoute = request()->routeIs('admin.*');
+                $isAdminMainRoute = request()->routeIs('admin.dashboard');
                 $dashboardRoute = $isAdminRoute ? route('admin.dashboard') : route('dashboard');
                 $dashboardLabel = $isAdminRoute ? __('nav.admin_dashboard') : __('nav.dashboard');
             @endphp
@@ -12,7 +13,9 @@
                 <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
                 </svg>
-                {{ $dashboardLabel }}
+                @if (!$isAdminMainRoute)
+                { $dashboardLabel }}
+                @endif
             </a>
         </li>
         @foreach ($items as $item)
